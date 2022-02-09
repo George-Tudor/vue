911f4501b7c139585c637524e9dd301cc540ea75
George Tudor <Alexander.George.Tudor@gmail.com>
2022-02-08 23:46:32 -0600

    starbase project using an api with vue

--- applications/product_page/main.js
+++ applications/product_page/main.js
@@ -1,7 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        infoLink: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
     }
 
 })
---
+++ starbase/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: 'Generate Your Team'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
