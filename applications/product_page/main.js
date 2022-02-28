3c89108b78f281d36475970b6545a2e05e68e33a
George Tudor <Alexander.George.Tudor@gmail.com>
2022-02-27 23:02:01 -0600

    v-for

--- applications/product_page/main.js
+++ applications/product_page/main.js
@@ -6,7 +6,19 @@ var app = new Vue({
         infoLink: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         description: 'A pair of warm, fuzzy socks',
         inStock: true,
-        onSale: true
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+            variantId: 2234,
+            variantColor: "green"
+           },
+           {
+               variantId: 2235,
+               variantColor: "blue"
+           }
+        ],
+        sizes: ["Small", "Medium", "Large", "X-Large", "XX-Large", "XXX-Large"]
+            // onSale: true
     }
 
 });
