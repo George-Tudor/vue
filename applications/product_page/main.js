80672fe27c123ba051e9de1937760cad7b7e4801
George Tudor <Alexander.George.Tudor@gmail.com>
2022-02-28 22:04:57 -0600

    class & style binding

--- applications/product_page/main.js
+++ applications/product_page/main.js
@@ -5,20 +5,34 @@ var app = new Vue({
         image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
         infoLink: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         description: 'A pair of warm, fuzzy socks',
-        inStock: true,
+        inStock: false,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
             variantId: 2234,
-            variantColor: "green"
+            variantColor: 'green',
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
            },
            {
                variantId: 2235,
-               variantColor: "blue"
+               variantColor: 'blue',
+               variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
            }
         ],
-        sizes: ["Small", "Medium", "Large", "X-Large", "XX-Large", "XXX-Large"]
-            // onSale: true
+        sizes: ["Small", "Medium", "Large", "X-Large", "XX-Large", "XXX-Large"],
+        cart: 0,
+        onSale: false
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        decreaseCart() {
+          this.cart -= 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 
 });
